5a69cd7b86aab07f4eadd1eaee82655aa96ccef8
Michael Drogalis <michael.drogalis@confluent.io>
2020-03-28 13:11:47 -0700

    Fixes Avro namespace collisions with nested data.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.mdrogalis/voluble "0.2.0"
+(defproject io.mdrogalis/voluble "0.2.1-SNAPSHOT"
   :description "Intelligent data generator for Apache Kafka. Generates streams of realistic data with support for cross-topic relationships, tombstoning, configurable rates, and more."
   :url "http://github.com/MichaelDrogalis/voluble"
   :license {:name "Eclipse Public License"
